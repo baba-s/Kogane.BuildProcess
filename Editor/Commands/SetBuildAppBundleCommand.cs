78b957410981954e36b699880be774aea0a7d748
baba_s <shotababa1206@gmail.com>
2020-08-01 14:18:01 +0900

    add commands

---
+++ Editor/Commands/SetBuildAppBundleCommand.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+
+namespace Kogane
+{
+	/// <summary>
+	/// apk ではなく aab をビルドするかどうかを設定するコマンドを管理するクラス
+	/// </summary>
+	public sealed class SetBuildAppBundleCommand : BuildCommandBase
+	{
+		//==============================================================================
+		// プロパティ
+		//==============================================================================
+		public override string Tag => nameof( SetBuildAppBundleCommand );
+
+		//==============================================================================
+		// 変数(readonly)
+		//==============================================================================
+		private readonly bool m_buildAppBundle;
+
+		//==============================================================================
+		// 変数
+		//==============================================================================
+		private bool m_oldBuildAppBundle;
+
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public SetBuildAppBundleCommand( bool buildAppBundle )
+		{
+			m_buildAppBundle = buildAppBundle;
+		}
+
+		/// <summary>
+		/// 実行する時に呼び出されます
+		/// </summary>
+		protected override BuildCommandResult DoRun()
+		{
+			m_oldBuildAppBundle                    = EditorUserBuildSettings.buildAppBundle;
+			EditorUserBuildSettings.buildAppBundle = m_buildAppBundle;
+			return Success( m_buildAppBundle.ToString() );
+		}
+
+		/// <summary>
+		/// 後始末します
+		/// </summary>
+		protected override void DoCleanUp()
+		{
+			EditorUserBuildSettings.buildAppBundle = m_oldBuildAppBundle;
+		}
+	}
+}
---
+++ Editor/Commands/SetWebGLTemplateStepCommand.cs
@@ -0,0 +1,55 @@
+﻿using Kogane;
+using UnityEditor;
+
+namespace Kogane
+{
+	/// <summary>
+	/// WebGL テンプレートを設定するコマンドを管理するクラス
+	/// </summary>
+	public sealed class SetWebGLTemplateCommand : BuildCommandBase
+	{
+		//==============================================================================
+		// プロパティ
+		//==============================================================================
+		public override string Tag => nameof( SetWebGLTemplateCommand );
+
+		//==============================================================================
+		// 変数(readonly)
+		//==============================================================================
+		private readonly string m_template;
+
+		//==============================================================================
+		// 変数
+		//==============================================================================
+		private string m_oldTemplate;
+
+		//==============================================================================
+		// 関数
+		//==============================================================================
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public SetWebGLTemplateCommand( string template )
+		{
+			m_template = template;
+		}
+
+		/// <summary>
+		/// 実行する時に呼び出されます
+		/// </summary>
+		protected override BuildCommandResult DoRun()
+		{
+			m_oldTemplate                 = PlayerSettings.WebGL.template;
+			PlayerSettings.WebGL.template = m_template;
+			return Success( m_template );
+		}
+
+		/// <summary>
+		/// 後始末します
+		/// </summary>
+		protected override void DoCleanUp()
+		{
+			PlayerSettings.WebGL.template = m_oldTemplate;
+		}
+	}
+}
